06b2fd28eddd8e267e47ed226fe1ccb3479275f7
Takahito Nakashima <19941015takahito@gmail.com>
2018-07-06 03:26:35 +0900

    set configuration

---
+++ sampleapp/src/main/java/sampleapp/config/AppConfig.java
@@ -0,0 +1,8 @@
+package sampleapp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ sampleapp/src/main/java/sampleapp/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package sampleapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("sample.app")
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp();
+	}
+
+}
